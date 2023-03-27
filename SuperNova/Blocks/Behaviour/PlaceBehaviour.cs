bc6c3259f938ac51ee0eec891fc81637f33f9d9a
RandomStrangers <110739593+RandomStrangers@users.noreply.github.com>
2023-03-27 12:32:09 -0400

    Update PlaceBehaviour.cs

--- SuperNova/Blocks/Behaviour/PlaceBehaviour.cs
+++ SuperNova/Blocks/Behaviour/PlaceBehaviour.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/SuperNova)
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/DeadNova)
     
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
@@ -15,11 +15,11 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
-using SuperNova.Blocks.Physics;
+using DeadNova.Blocks.Physics;
 using System;
 using BlockID = System.UInt16;
 
-namespace SuperNova.Blocks {
+namespace DeadNova.Blocks {
     
     internal static class PlaceBehaviour {
 
