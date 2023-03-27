f88aa3101971e257cbafe1ce770119638190d842
RandomStrangers <110739593+RandomStrangers@users.noreply.github.com>
2023-03-27 12:37:44 -0400

    Update Updater.cs

--- SuperNova/Server/Maintenance/Updater.cs
+++ SuperNova/Server/Maintenance/Updater.cs
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/SuperNova)
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/DeadNova)
     
     Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
@@ -20,26 +20,26 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Reflection;
-using SuperNova.Network;
-using SuperNova.Tasks;
+using DeadNova.Network;
+using DeadNova.Tasks;
 
-namespace SuperNova {
+namespace DeadNova {
     /// <summary> Checks for and applies software updates. </summary>
     public static class Updater {
         
         public static string SourceURL = "https://github.com/RandomStrangers/SuperNova-DeadNova/";
-        public const string BaseURL    = "https://github.com/RandomStrangers/SuperNova-DeadNova/blob/master/";
-        public const string UploadsURL = "https://github.com/RandomStrangers/SuperNova-DeadNova/tree/master/Uploads";
+        public const string BaseURL    = "https://github.com/RandomStrangers/SuperNova-DeadNova/blob/DeadNova/";
+        public const string UploadsURL = "https://github.com/RandomStrangers/SuperNova-DeadNova/tree/DeadNova/Uploads";
         
         const string CurrentVersionURL = BaseURL + "Uploads/current_version.txt";
         #if TEN_BIT_BLOCKS
-        const string dllURL = BaseURL + "Uploads/SuperNova_infid.dll?raw=true";
+        const string dllURL = BaseURL + "Uploads/DeadNova_infid.dll?raw=true";
         #else
-        const string dllURL = BaseURL + "Uploads/SuperNova_.dll?raw=true";
+        const string dllURL = BaseURL + "Uploads/DeadNova_.dll?raw=true";
         #endif
         const string changelogURL = BaseURL + "Changelog.txt";
-        const string guiURL = BaseURL + "Uploads/SuperNova.exe?raw=true";
-        const string cliURL = BaseURL + "Uploads/SuperNovaCLI.exe?raw=true";
+        const string guiURL = BaseURL + "Uploads/DeadNova.exe?raw=true";
+        const string cliURL = BaseURL + "Uploads/DeadNovaCLI.exe?raw=true";
 
         public static event EventHandler NewerVersionDetected;
         
@@ -70,15 +70,15 @@ namespace SuperNova {
         public static void PerformUpdate() {
             try {
                 try {
-                    DeleteFiles("Changelog.txt", "SuperNova_.update", "SuperNova.update", "SuperNovaCLI.update",
-                                "prev_SuperNova_.dll", "prev_SuperNova.exe", "prev_SuperNovaCLI.exe");
+                    DeleteFiles("Changelog.txt", "DeadNova_.update", "DeadNova.update", "DeadNovaCLI.update",
+                                "prev_DeadNova_.dll", "prev_DeadNova.exe", "prev_DeadNovaCLI.exe");
                 } catch {
                 }
                 
                 WebClient client = HttpUtil.CreateWebClient();
-                client.DownloadFile(dllURL, "SuperNova_.update");
-                client.DownloadFile(guiURL, "SuperNova.update");
-                client.DownloadFile(cliURL, "SuperNovaCLI.update");
+                client.DownloadFile(dllURL, "DeadNova_.update");
+                client.DownloadFile(guiURL, "DeadNova.update");
+                client.DownloadFile(cliURL, "DeadNovaCLI.update");
                 client.DownloadFile(changelogURL, "Changelog.txt");
 
                 Level[] levels = LevelInfo.Loaded.Items;
@@ -93,14 +93,14 @@ namespace SuperNova {
                 
                 // Move current files to previous files (by moving instead of copying, 
                 //  can overwrite original the files without breaking the server)
-                AtomicIO.TryMove("SuperNova_.dll", "prev_SuperNova_.dll");
-                AtomicIO.TryMove("SuperNova.exe", "prev_SuperNova.exe");
-                AtomicIO.TryMove("SuperNovaCLI.exe", "prev_SuperNovaCLI.exe");
+                AtomicIO.TryMove("DeadNova_.dll", "prev_DeadNova_.dll");
+                AtomicIO.TryMove("DeadNova.exe", "prev_DeadNova.exe");
+                AtomicIO.TryMove("DeadNovaCLI.exe", "prev_DeadNovaCLI.exe");
                 
                 // Move update files to current files
-                File.Move("SuperNova_.update",   "SuperNova_.dll");
-                File.Move("SuperNova.update",    "SuperNova.exe");
-                File.Move("SuperNovaCLI.update", "SuperNovaCLI.exe");                             
+                File.Move("DeadNova_.update",   "DeadNova_.dll");
+                File.Move("DeadNova.update",    "DeadNova.exe");
+                File.Move("DeadNovaCLI.update", "DeadNovaCLI.exe");                             
 
                 Server.Stop(true, "Updating server.");
             } catch (Exception ex) {
