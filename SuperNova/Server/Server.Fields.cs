f28068c215108764735416188cf3e9cb1b123352
RandomStrangers <110739593+RandomStrangers@users.noreply.github.com>
2023-03-27 12:34:50 -0400

    Update Server.Fields.cs

--- SuperNova/Server/Server.Fields.cs
+++ SuperNova/Server/Server.Fields.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/SuperNova)
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/DeadNova)
     
     Dual-licensed under the    Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
